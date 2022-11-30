3d3c47f8e616927bcb5832d7bf9d6bbee249b335
DanielMarcoD <danielmd3@al.insper.edu.br>
2022-11-30 18:30:28 -0300

    tentativa

--- DOC.py
+++ DOC.py
@@ -10,11 +10,11 @@ options_fonte = pygame.font.SysFont('Arial', 40)                  # Fonte para a
 
 tela_inicial = pygame.display.set_mode((500, 530), 0, 32)           # Janela
 pygame.display.set_caption("DOC")                            # Titulo da janela
-tela_icone = pygame.image.load('assets/logo2.png')                       # Imagem do icone do jogo
+tela_icone = pygame.image.load('assets/logo2.png').convert()                       # Imagem do icone do jogo
 pygame.display.set_icon(tela_icone)
 
-tela_de_fundo = pygame.image.load('assets/DOC.png')                     # Imagem de fundo
-tela_jogo = pygame.image.load('assets/Fundo.png')
+tela_de_fundo = pygame.image.load('assets/DOC.png').convert()                     # Imagem de fundo
+tela_jogo = pygame.image.load('assets/Fundo.png').convert()
 
 # Cores
 VERMELHO = (255, 0, 0)
